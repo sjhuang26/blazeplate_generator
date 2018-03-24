c1e54082b84002c36c6767d4e1236899c0810b88
Alexander Schwartzberg <aeksco@gmail.com>
2018-03-24 01:38:41 -0400

    Added Vue beautify helper to test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
+
+# Blazeplate generate
 yo blazeplate --force --appconfig=./configs/example_01.json
+
+# JS Beautify
 glob-run js-beautify --max_preserve_newlines 1 -r -s 2 'generated_apps/app_name/web_api/server/**/*.js'
 
-# '**/*.html'
-# glob-run cat src/\*.js
+# Vue Beautify
+# glob-run vue-beautify -r --max_preserve_newlines=0 -s=2 -a=1 -S=keep 'generated_apps/app_name/web_client/src/containers/**/*.vue'
