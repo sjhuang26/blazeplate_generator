1c020ba2a912a76c3da727e97697ab0bde3888c6
Alexander Schwartzberg <aeksco@gmail.com>
2018-03-23 23:14:49 -0400

    Added APIDoc headers to resource.controller, cleaned up resource.model code

--- generators/expressjs/expressjs_resource/templates/resource.model.js
+++ generators/expressjs/expressjs_resource/templates/resource.model.js
@@ -4,38 +4,38 @@ const Schema = mongoose.Schema
 // // // //
 
 const <%= schema.label.split(' ').join('') %> = new Schema({
-    <% for (index in schema.attributes) { %>
-    <% let attr = schema.attributes[index] %>
-    <% if (attr.datatype === 'BOOL') { %>
-    <%= attr.identifier %>: {
-        type: Boolean
-    },
-    <% } else if (attr.datatype === 'NUMBER') { %>
-    <%= attr.identifier %>: {
-        type: Number
-    },
-    <% } else if (attr.datatype === 'BELONGS_TO') { %>
-    <%= attr.identifier %>: {
-        type: Schema.Types.ObjectId,
-        ref: '<%= attr.datatypeOptions.schema_label %>'
-    },
-    <% } else { %>
-    <%= attr.identifier %>: {
-        type: String
-    },
-    <% } %>
-    <% } %>
-},
-    // Collection options
-    {
-        timestamps: {
-            createdAt: 'createdOn',
-            updatedAt: 'updatedOn'
-        },
-        versionKey: false
-    }
-);
+  <% for (index in schema.attributes) { %>
+  <% let attr = schema.attributes[index] %>
+  <% if (attr.datatype === 'BOOL') { %>
+  <%= attr.identifier %>: {
+    type: Boolean
+  },
+  <% } else if (attr.datatype === 'NUMBER') { %>
+  <%= attr.identifier %>: {
+    type: Number
+  },
+  <% } else if (attr.datatype === 'BELONGS_TO') { %>
+  <%= attr.identifier %>: {
+    type: Schema.Types.ObjectId,
+    ref: '<%= attr.datatypeOptions.schema_label %>'
+  },
+  <% } else { %>
+  <%= attr.identifier %>: {
+    type: String
+ },
+  <% } %>
+  <% } %>
+  },
+  // Collection options
+  {
+    timestamps: {
+      createdAt: 'createdOn',
+      updatedAt: 'updatedOn'
+  },
+  versionKey: false
+});
 
 // // // //
 
+// TODO - absract schema.label.split(' ').join('')
 module.exports = mongoose.model('<%= schema.label.split(' ').join('') %>', <%= schema.label.split(' ').join('') %>)
