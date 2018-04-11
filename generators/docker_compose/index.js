d2cb2b68add8351b04a7af24ce7a621e1e060cb9
Alexander Schwartzberg <aeksco@gmail.com>
2018-04-11 12:23:36 -0400

    Expanded scope of Docker-Compose file generation

--- generators/docker_compose/index.js
+++ generators/docker_compose/index.js
@@ -2,17 +2,28 @@
 var Generator = require('yeoman-generator')
 
 module.exports = class extends Generator {
-
-  // writing to file
   writing() {
 
-    // generated/docker-compose.yml
+    // generated/docker-compose-dev.yml
     this.fs.copyTpl(
-      this.templatePath('docker-compose.yml'),
-      this.destinationPath(this.options.build.dest.root + 'docker-compose.yml'),
+      this.templatePath('docker-compose-dev.yml'),
+      this.destinationPath(this.options.build.dest.root + 'docker-compose-dev.yml'),
+      {}
+    )
+
+    // generated/docker-compose-demo.yml
+    this.fs.copyTpl(
+      this.templatePath('docker-compose-demo.yml'),
+      this.destinationPath(this.options.build.dest.root + 'docker-compose-demo.yml'),
+      {}
+    )
+
+    // generated/demo.sh
+    this.fs.copyTpl(
+      this.templatePath('demo.sh'),
+      this.destinationPath(this.options.build.dest.root + 'demo.sh'),
       {}
     )
 
   }
-
 }
---
+++ generators/docker_compose/templates/demo.sh
@@ -0,0 +1,5 @@
+# Copies demo compose file
+cp ./docker-compose-demo.yml ./docker-compose.yml
+
+# Starts docker-compose
+docker-compose up -d
