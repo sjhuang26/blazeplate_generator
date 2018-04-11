d5459d5470dbc2ff50d2e6bcc3263cfbe425da4f
Alexander Schwartzberg <aeksco@gmail.com>
2018-04-11 19:18:52 -0400

    Added npm run dev task, cleaned up resource router in expressjs

--- generators/expressjs/expressjs_resource/templates/index.js
+++ generators/expressjs/expressjs_resource/templates/index.js
@@ -1,20 +1,26 @@
 const router = require('express').Router();
 const controller = require('./<%= schema.identifier %>.controller');
 
+// // // // BLAZEPLATE WHITESPACE
 // // // //
 
+// // // // BLAZEPLATE WHITESPACE
 // GET /<%= schema.identifier_plural %>
 router.get('/', controller.list);
 
+// // // // BLAZEPLATE WHITESPACE
 // POST /<%= schema.identifier_plural %>
 router.post('/', controller.create);
 
+// // // // BLAZEPLATE WHITESPACE
 // GET /<%= schema.identifier_plural %>/:id
 router.get('/:id', controller.show);
 
+// // // // BLAZEPLATE WHITESPACE
 // PUT /<%= schema.identifier_plural %>/:id
 router.put('/:id', controller.update);
 
+// // // // BLAZEPLATE WHITESPACE
 // DELETE /<%= schema.identifier_plural %>/:id
 router.delete('/:id', controller.delete);
 
@@ -22,20 +28,26 @@ router.delete('/:id', controller.delete);
 <% let attr = schema.attributes[index] %>
 
 <% if (attr.datatype === 'RELATION' && attr.datatypeOptions.relationType === 'BELONGS_TO') { %>
+// // // // BLAZEPLATE WHITESPACE
 // GET /<%= schema.identifier_plural %>/:id/<%= attr.datatypeOptions.schema_identifier %>
 router.get('/:id/<%= attr.datatypeOptions.schema_identifier %>', controller.show<%= attr.datatypeOptions.schema_label %>);
 
 <% } else if (attr.datatype === 'RELATION' && attr.datatypeOptions.relationType === 'HAS_MANY') { %>
+// // // // BLAZEPLATE WHITESPACE
 // GET /<%= schema.identifier_plural %>/:id/<%= attr.datatypeOptions.schema_identifier_plural %>
 router.get('/:id/<%= attr.datatypeOptions.schema_identifier_plural %>', controller.show<%= attr.datatypeOptions.schema_label_plural %>);
 
 <% } else if (attr.datatype === 'RELATION' && attr.datatypeOptions.relationType === 'HAS_ONE') { %>
+// // // // BLAZEPLATE WHITESPACE
 // GET /<%= schema.identifier_plural %>/:id/<%= attr.datatypeOptions.schema_identifier %>
 router.get('/:id/<%= attr.datatypeOptions.schema_identifier %>', controller.show<%= attr.datatypeOptions.schema_label %>);
 
 
 <% } %>
 <% } %>
+
+// // // // BLAZEPLATE WHITESPACE
 // // // //
+// // // // BLAZEPLATE WHITESPACE
 
 module.exports = router;
