28a82df2bc87606674aea46df3aa94ac4dfccbde
Alexander Schwartzberg <aeksco@gmail.com>
2018-04-11 23:12:15 -0400

    Updates to vuex store actions

--- generators/vuejs/vuejs_store/templates/actions.js
+++ generators/vuejs/vuejs_store/templates/actions.js
@@ -1,17 +1,24 @@
-import Factory from './factory'
+import { $GET, $POST, $PUT, $DELETE } from '@/store/lib/helpers'
 
 // // // //
 
 // actions
 // functions that causes side effects and can involve asynchronous operations.
 const actions = {
-  fetchCollection: ({ commit }) => Factory.fetchCollection({ commit }),
-
-  create: ({ commit }, attributes) => Factory.create({ commit }, attributes),
-
-  update: ({ commit }, attributes) => Factory.update({ commit }, attributes),
-
-  destroy: ({ commit }, id) => Factory.destroy({ commit }, id)
+  // fetchCollection: ({ commit }) => Factory.fetchCollection({ commit }),
+  // create: ({ commit }, attributes) => Factory.create({ commit }, attributes),
+  // update: ({ commit }, attributes) => Factory.update({ commit }, attributes),
+  // destroy: ({ commit }, id) => Factory.destroy({ commit }, id)
+  fetchCollection ({ state, commit, dispatch }) {
+  },
+  fetchModel ({ state, commit, dispatch }) {
+  },
+  createModel ({ state, commit, dispatch }) {
+  },
+  updateModel ({ state, commit, dispatch }) {
+  },
+  deleteModel ({ state, commit, dispatch }) {
+  },
 }
 
 // // // //
