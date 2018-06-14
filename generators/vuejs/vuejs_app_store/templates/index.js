ccffaea13e4636588b763208d003c46ca33fd607
Alexander Schwartzberg <aeksco@gmail.com>
2018-06-14 15:22:56 -0400

    Removed whitespace markers from vuejs_store index.js

--- generators/vuejs/vuejs_app_store/templates/index.js
+++ generators/vuejs/vuejs_app_store/templates/index.js
@@ -1,16 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+
 // TODO - auth should be dynamicallly included
 import auth from './auth'
 import notification from './notification'
-<% for (index in appSchema.schemas) { %>
+<%_ for (index in appSchema.schemas) { %>
 import <%= appSchema.schemas[index].identifier %> from './<%= appSchema.schemas[index].identifier %>'
-<% } %>
-// // // // BLAZEPLATE WHITESPACE
+<%_ } %>
+
 // Vuex Initialization
 // TODO - should this be done elsewhere?
 Vue.use(Vuex)
-// // // // BLAZEPLATE WHITESPACE
+
 export default new Vuex.Store({
   modules: {
     auth,
@@ -18,4 +19,3 @@ export default new Vuex.Store({
     <%= storeModules %>
   }
 })
-// // // // BLAZEPLATE WHITESPACE
