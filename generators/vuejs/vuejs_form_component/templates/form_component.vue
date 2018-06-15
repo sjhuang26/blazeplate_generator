130f3d2945f7beeb1cdef903d8d6b5211c69bd21
Alexander Schwartzberg <aeksco@gmail.com>
2018-06-15 13:52:06 -0400

    Refined VueJS form component template

--- generators/vuejs/vuejs_form_component/templates/form_component.vue
+++ generators/vuejs/vuejs_form_component/templates/form_component.vue
@@ -1,37 +1,39 @@
 
 <template>
   <div class='row'>
-  <% for (index in schema.attributes) { %>
-  <% let attr = schema.attributes[index] %>
+  <%_ for (index in schema.attributes) { _%>
+  <%_ let attr = schema.attributes[index] _%>
     <div class="col-lg-6">
       <div class="form-group">
         <label class='mb-0'>
           <%= attr.label %>
-          <% if (attr.required) { %><span class='text-danger'>*</span><% } %>
+          <%_ if (attr.required) { %><span class='text-danger'>*</span><% } _%>
         </label>
         <small class="form-text text-muted mb-2"><%= attr.help %></small>
-      <% if (attr.datatype === 'BOOL') { %>
+      <%_ if (attr.datatype === 'BOOL') { _%>
         <input type="checkbox" class="form-control" v-model="model.<%=attr.identifier%>">
-      <% } else if (attr.datatype === 'TEXT') { %>
+      <%_ } else if (attr.datatype === 'TEXT') { _%>
         <input type="text" class="form-control" placeholder="<%= attr.label %>" v-model="model.<%=attr.identifier%>">
-      <% } else if (attr.datatype === 'NUMBER') { %>
+      <%_ } else if (attr.datatype === 'NUMBER') { _%>
         <input type="number" class="form-control" placeholder="<%= attr.label %>" v-model="model.<%=attr.identifier%>">
-      <% } else if (attr.datatype === 'DATE') { %>
+      <%_ } else if (attr.datatype === 'DATE') { _%>
         <input type="date" class="form-control" placeholder="<%= attr.label %>" v-model="model.<%=attr.identifier%>">
-      <% } else if (attr.datatype === 'RELATION') { %>
+      <%_ } else if (attr.datatype === 'RELATION') { _%>
 
-      <% } if (attr.datatypeOptions.relationType === 'BELONGS_TO') { %>
+      <%_ } if (attr.datatypeOptions.relationType === 'BELONGS_TO') { _%>
         <select type="text" class="form-control" placeholder="<%= attr.label %>" v-model="model.<%=attr.identifier%>">
-          <option :value="<%=attr.datatypeOptions.schema_identifier%>._id" v-for="<%=attr.datatypeOptions.schema_identifier%> in <%= attr.datatypeOptions.schema_identifier_plural %>">{{ <%= attr.datatypeOptions.schema_identifier %>.<%= attr.datatypeOptions.lead_attr %> }}</option>
+          <option :value="<%=attr.datatypeOptions.schema_identifier%>._id" v-for="<%=attr.datatypeOptions.schema_identifier%> in <%= attr.datatypeOptions.schema_identifier_plural %>">
+            {{ <%= attr.datatypeOptions.schema_identifier %>.<%= attr.datatypeOptions.lead_attr %> }}
+          </option>
         </select>
-      <% } else if (attr.datatypeOptions.relationType === 'HAS_MANY') { %>
+      <%_ } else if (attr.datatypeOptions.relationType === 'HAS_MANY') { _%>
         <select type="text" multiple class="form-control" placeholder="<%= attr.label %>" v-model="model.<%=attr.identifier%>">
           <option :value="<%=attr.datatypeOptions.schema_identifier%>._id" v-for="<%=attr.datatypeOptions.schema_identifier%> in <%= attr.datatypeOptions.schema_identifier_plural %>">{{ <%= attr.datatypeOptions.schema_identifier %>.<%= attr.datatypeOptions.lead_attr %> }}</option>
         </select>
-      <% } %>
+      <%_ } _%>
       </div>
     </div>
-  <% } %>
+  <%_ } _%>
 
   </div>
 </template>
@@ -49,22 +51,22 @@ export default {
     RelationDropdown
   },
   created () {
-    <% for (index in schema.attributes) { %>
-    <% let attr = schema.attributes[index] %>
-    <% if (attr.datatype === 'RELATION') { %>
+    <%_ for (index in schema.attributes) { _%>
+    <%_ let attr = schema.attributes[index] _%>
+    <%_ if (attr.datatype === 'RELATION') { _%>
     this.$store.dispatch('<%= attr.datatypeOptions.schema_identifier %>/fetchCollection')
-    <% } %>
-    <% } %>
+    <%_ } _%>
+    <%_ } _%>
   },
   computed: {
-    <% for (index in schema.attributes) { %>
-    <% let attr = schema.attributes[index] %>
-    <% if (attr.datatype === 'RELATION') { %>
+    <%_ for (index in schema.attributes) { _%>
+    <%_ let attr = schema.attributes[index] _%>
+    <%_ if (attr.datatype === 'RELATION') { _%>
     <%=attr.datatypeOptions.schema_identifier_plural%> () {
       return this.$store.getters['<%= attr.datatypeOptions.schema_identifier %>/collection']
     },
-    <% } %>
-    <% } %>
+    <%_ } _%>
+    <%_ } _%>
   }
 }
 </script>
