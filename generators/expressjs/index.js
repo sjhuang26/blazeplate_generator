9716744500166da11b8a22594f192399335c60a9
Alexander Schwartzberg <aeksco@gmail.com>
2018-06-17 13:46:28 -0400

    Added ExpressJSResourceSpec generator to root-level ExpressJS generator

--- generators/expressjs/index.js
+++ generators/expressjs/index.js
@@ -1,6 +1,7 @@
 const Generator = require('../util/generator')
 const ExpressApp = require('./expressjs_app')
 const ExpressResource = require('./expressjs_resource')
+const ExpressResourceSpec = require('./expressjs_resource_spec')
 const ExpressRoutes = require('./expressjs_routes')
 
 module.exports = class ExpressJS extends Generator {
@@ -8,5 +9,6 @@ module.exports = class ExpressJS extends Generator {
     await this.composeWith(ExpressApp)
     await this.composeWith(ExpressResource)
     await this.composeWith(ExpressRoutes)
+    await this.composeWith(ExpressResourceSpec)
   }
 }
