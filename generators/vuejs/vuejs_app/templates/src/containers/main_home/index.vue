7a6029bac88883ed0631ebca4cd14e0d0309dc29
Alexander Schwartzberg <aeksco@gmail.com>
2018-06-20 12:54:23 -0400

    VueJS main home container links to blazeplate documentation

--- generators/vuejs/vuejs_app/templates/src/containers/main_home/index.vue
+++ generators/vuejs/vuejs_app/templates/src/containers/main_home/index.vue
@@ -18,7 +18,7 @@
 
       <div class="row my-4">
         <div class="col-lg-12 text-center">
-          <a class="btn btn-lg btn-outline-dark" href="https://github.com/blazeplate/blazeplate" target="_blank">
+          <a class="btn btn-lg btn-outline-dark" href="https://blazeplate.github.io" target="_blank">
             <i class="fa fa-book mr-1"></i>
             Documentation
           </a>
