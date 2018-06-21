0f7ded3651a34e8cf2e756b96d45256778b82b81
Alexander Schwartzberg <aeksco@gmail.com>
2018-06-21 19:15:07 -0400

    Removed broken example JSON files

--- blazeplate.js
+++ blazeplate.js
@@ -1,7 +1,7 @@
 let Blazeplate = require('./generators/app')
 
-// Invoke Blazeplate directly
+// Invoke Blazeplate directly with the ToDo List example
 new Blazeplate({
-  appconfig: require('./configs/example_04.json'),
+  appconfig: require('./examples/todo-list.json'),
   buildId: 'app_5acfeea85535afdb753d55f7'
 }).write()
