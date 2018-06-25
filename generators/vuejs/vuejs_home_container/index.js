50e69df11670db2ba32bff4f90b386babd8f8263
Alexander Schwartzberg <aeksco@gmail.com>
2018-06-25 17:34:03 -0400

    Simplified vuejs_home_container generator

--- generators/vuejs/vuejs_home_container/index.js
+++ generators/vuejs/vuejs_home_container/index.js
@@ -8,20 +8,15 @@ module.exports = class VueJsHomeContainer extends Generator {
     let { ui_framework } = this.options.build.app.stack
     let options = { ui_framework }
 
-    // Bootstrap or Onsen?
-    let appTemplate = ''
-    if (options.ui_framework.id === 'bootstrap') {
-      appTemplate = 'home.bootstrap.vue'
-    } else if (options.ui_framework.id === 'onsenui') {
-      appTemplate = 'home.onsen.vue'
-    }
+    // Bootstrap / Onsen UI framework
+    let appTemplate = `home.${options.ui_framework.id}.vue`
 
     // web_client/src/containers/main_home/index.vue
     await this.copyTemplate(
       this.templatePath(__dirname, appTemplate),
       this.destinationPath(this.options.build.dest.vue.src + 'containers/main_home/index.vue'),
       { }
-    );
+    )
 
   }
 }
