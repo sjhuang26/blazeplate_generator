1203e0ad7cdd523cf4b21861812af3ccff6fb0b5
sjhuang26 <sjhuang26@gmail.com>
2018-06-26 19:26:08 -0400

    Refactor vuejs_new_container

--- generators/vuejs/vuejs_show_container/templates/partials/script-tag.vue
+++ generators/vuejs/vuejs_show_container/templates/partials/script-tag.vue
@@ -1,29 +1,3 @@
-
-<template>
-  <div class="container">
-
-    <!-- ADD SHOW WIDGET BACK HERE -->
-    <<%= schema.label %>ShowWidget :model="model" :fetching="fetching" />
-
-    <div class="row">
-      <%_ for (index in schema.relations) { _%>
-      <%_ let relation = schema.relations[index] _%>
-      <div class="col-lg-12 mt-2">
-
-        <%_ if (relation.type === 'LIST') { _%>
-        <<%= relation.name %> :header="'<%= relation.name %>'" :fetching="false" :collection="<%= relation.getter %>" />
-        <%_ } else { _%>
-        <<%= relation.name %> :header="'<%= relation.name %>'" :fetching="false" :model="<%= relation.getter %>" />
-        <%_ } _%>
-      </div>
-      <%_ } _%>
-    </div>
-
-  </div>
-</template>
-
-<!-- // // // //  -->
-
 <script>
 import { mapGetters, mapActions } from 'vuex'
 import Loading from '@/components/Loading'
---
+++ generators/vuejs/vuejs_show_container/templates/show_container.vue
@@ -0,0 +1,9 @@
+<%_ if (options.ui_framework.id === 'bootstrap') { _%>
+<%- include partials/template-tag-bootstrap.vue %>
+<%_ } else { _%>
+<%- include partials/template-tag-no-ui.vue %>
+<%_ } _%>
+
+<!-- // // // //  -->
+
+<%- include partials/script-tag.vue %>
