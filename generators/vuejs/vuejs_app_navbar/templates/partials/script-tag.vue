0812a90d53ba473e62547c4de79645bc45fba471
sjhuang26 <sjhuang26@gmail.com>
2018-07-02 21:39:32 -0400

    Refactored vuejs_app_navbar

---
+++ generators/vuejs/vuejs_app_navbar/templates/partials/script-tag.vue
@@ -0,0 +1,15 @@
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'Navbar',
+  computed: mapGetters({
+    isAuthenticated: 'auth/is_authenticated',
+    currentUser: 'auth/current_user'
+  }),
+  methods: mapActions({
+    logout: 'auth/logout'
+  })
+}
+</script>
+
