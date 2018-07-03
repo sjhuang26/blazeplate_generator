22d0785ddc1795fadbba60daa559ad9c6138d9b1
sjhuang26 <sjhuang26@gmail.com>
2018-07-02 21:39:54 -0400

    Refactor vuejs_edit_container

---
+++ generators/vuejs/vuejs_edit_container/templates/partials/template-tag-no-ui.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2><%= schema.label %> - Edit</h2>
+
+    <hr>
+
+    <<%= schema.label %>Form :model="model" v-if="model._id && !fetching" />
+    <Loading v-else />
+
+    <a href="#/<%= schema.identifier_plural %>">
+      <i class="fa fa-fw fa-times mr-1"></i>
+      Cancel
+    </a>
+
+    <button @click="formSubmit(model)">
+      <i class="fa fa-fw fa-plus mr-1"></i>
+      Update <%= schema.label %>
+    </button>
+  </div>
+</template>
+
