e1fbe024b2ec995eaac19c50e7cc1b8bcebf457a
Alexander Schwartzberg <aeksco@gmail.com>
2018-07-05 18:14:53 -0400

    Added basic reactjs generator - reactjs_app_base copies and runs successfully!

--- generators/app/generators.js
+++ generators/app/generators.js
@@ -3,6 +3,7 @@
 module.exports = {
   ExpressJS: require('../expressjs'),
   VueJS: require('../vuejs'),
+  ReactJS: require('../reactjs'),
   SeedData: require('../seed_data'),
   DockerCompose: require('../docker_compose')
 }
