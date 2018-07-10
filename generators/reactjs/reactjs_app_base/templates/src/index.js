2f2c8d736047b192f49b7d4777f9bd5c2c156c80
sjhuang26 <sjhuang26@gmail.com>
2018-07-10 15:46:32 -0400

    Create initial prototype of react-router

--- generators/reactjs/reactjs_app_base/templates/src/index.js
+++ generators/reactjs/reactjs_app_base/templates/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
+
