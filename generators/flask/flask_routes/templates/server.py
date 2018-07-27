b29fd7f175ca89648488dc2b53fe6674074ae16a
Alexander Schwartzberg <aeksco@gmail.com>
2018-07-27 19:09:22 -0400

    Scoped all flask routes under /api

--- generators/flask/flask_routes/templates/server.py
+++ generators/flask/flask_routes/templates/server.py
@@ -17,26 +17,23 @@ from resources.<%= schema.identifier_plural %> import *
 
 # # # # #
 
-
 # Assign the Flask instance (a WSGI application) to the app variable.
 app = Flask(__name__)
 
-
 # Add routes to resources
 <% for (let schema of appSchema.schemas) { -%>
-app.add_url_rule('/<%- schema.identifier_plural %>', view_func=<%- schema.class_name %>CollectionResource.as_view('<%- schema.identifier_plural %>_collection_resource'), methods=['GET', 'POST'])
-app.add_url_rule('/<%- schema.identifier_plural %>/<<%- schema.identifier %>_id>', view_func=<%- schema.class_name %>ModelResource.as_view('<%- schema.identifier_plural %>_model_resource'), methods=['GET', 'PUT', 'DELETE'])
+app.add_url_rule('/api/<%- schema.identifier_plural %>', view_func=<%- schema.class_name %>CollectionResource.as_view('<%- schema.identifier_plural %>_collection_resource'), methods=['GET', 'POST'])
+app.add_url_rule('/api/<%- schema.identifier_plural %>/<<%- schema.identifier %>_id>', view_func=<%- schema.class_name %>ModelResource.as_view('<%- schema.identifier_plural %>_model_resource'), methods=['GET', 'PUT', 'DELETE'])
 <% for (let attr of schema.attributes) { -%>
 <% if (attr.datatype === 'RELATION') { -%>
 <% if (attr.datatypeOptions.relationType === 'BELONGS_TO') { -%>
-app.add_url_rule('/<%- schema.identifier_plural %>/<<%- schema.identifier %>_id>/<%- attr.datatypeOptions.schema_identifier %>', view_func=<%- schema.class_name %>Related<%- attr.datatypeOptions.schema_class_name %>Resource.as_view('<%- schema.identifier %>_related_<%- attr.datatypeOptions.schema_identifier %>_resource'), methods=['GET'])
+app.add_url_rule('/api/<%- schema.identifier_plural %>/<<%- schema.identifier %>_id>/<%- attr.datatypeOptions.schema_identifier %>', view_func=<%- schema.class_name %>Related<%- attr.datatypeOptions.schema_class_name %>Resource.as_view('<%- schema.identifier %>_related_<%- attr.datatypeOptions.schema_identifier %>_resource'), methods=['GET'])
 <% } else if (attr.datatypeOptions.relationType === 'OWNS_MANY' || attr.datatypeOptions.relationType === 'HAS_MANY') { -%>
-app.add_url_rule('/<%- schema.identifier_plural %>/<<%- schema.identifier %>_id>/<%- attr.datatypeOptions.schema_identifier_plural %>', view_func=<%- schema.class_name %>Related<%- attr.datatypeOptions.schema_class_name_plural %>Resource.as_view('<%- schema.identifier %>_related_<%- attr.datatypeOptions.schema_identifier_plural %>_resource'), methods=['GET'])
+app.add_url_rule('/api/<%- schema.identifier_plural %>/<<%- schema.identifier %>_id>/<%- attr.datatypeOptions.schema_identifier_plural %>', view_func=<%- schema.class_name %>Related<%- attr.datatypeOptions.schema_class_name_plural %>Resource.as_view('<%- schema.identifier %>_related_<%- attr.datatypeOptions.schema_identifier_plural %>_resource'), methods=['GET'])
 <% } -%>
 <% } -%>
 <% } -%>
 <% } -%>
-
 
 if __name__ == '__main__':
     app.run(debug=True)
