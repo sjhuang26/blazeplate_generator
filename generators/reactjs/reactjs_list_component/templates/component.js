bec0969178dd47719e0b45732bb89609ccc867de
sjhuang26 <sjhuang26@gmail.com>
2018-08-06 11:04:42 -0400

    React: add functionality to list component

--- generators/reactjs/reactjs_list_component/templates/component.js
+++ generators/reactjs/reactjs_list_component/templates/component.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+import Loader from '../components/Loader';
+import <%- schema.class_name %>Preview from './<%- schema.class_name %>Preview';
+
 class <%- schema.class_name %>List extends Component {
   constructor(props) {
     super(props)
@@ -9,54 +12,91 @@ class <%- schema.class_name %>List extends Component {
       collection: [],
       loading: false
     }
+    this.deleteItem = this.deleteItem.bind(this);
   }
 
   componentDidMount() {
-    this.setState({ loading: true })
-
-    axios.get('/api/<%- schema.identifier_plural %>')
-    .then((response) => {
-      this.setState({
-        collection: response.data,
-        loading: false
-      })
-    })
+    this.updateCollection();
   }
+
+  updateCollection() {
+    this.setState({ loading: true })
+    axios.get('/api/<%- schema.identifier_plural %>')
+      .then((response) => {
+        this.setState({
+          collection: response.data,
+          loading: false
+        })
+      })
+  }
+
+  deleteItem(id) {
+    axios.delete('/api/<%- schema.identifier_plural %>/' + id)
+      .then(() => {
+        this.updateCollection();
+      })
+  }
+
   render() {
-
-    let content
-
-    // Show loading view
-    if (this.state.loading) {
-      content = <i className='fa fa-3x fa-spin fa-spinner'></i>;
-    // Show collection
-    } else if (this.state.collection.length) {
-
-      let rows = []
-      for (var i = 0; i < this.state.collection.length; i++) {
-          // note: we add a key prop here to allow react to uniquely identify each
-          // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
-          let model = this.state.collection[i]
-          let showUrl = "/<%= schema.identifier_plural %>/" + model._id
-          rows.push(
-            <Link key={model._id} className='list-group-item' to={showUrl}>{ model.label }</Link>);
-      }
-      content = <ul className='list-group'>{rows}</ul>;
-    // Show empty message
-    } else {
-      content = <ul className='list-group'><li className='list-group-item list-group-item-warning'>No <%- schema.label_plural %> available</li></ul>;
-    }
-
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-lg-12'>
-            <h2><%- schema.label_plural %></h2>
-            <Link className="btn btn-primary" to="/<%- schema.identifier_plural %>/new">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8">
+            <h2>
+              <%= schema.label_plural %>
+            </h2>
+          </div>
+
+          <div className="col-md-4 text-right">
+            <Link className="btn btn-outline-success" to="/<%- schema.identifier_plural %>/new">
+              <i className="fa fa-fw fa-plus mr-2"></i>
               New <%- schema.label %>
             </Link>
-            <hr/>
-            {content}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <Loader isLoaded={!this.state.loading}>
+              <table className="table table-striped table-hover">
+                <thead>
+                  <tr>
+                    <%_ for (let attr of schema.attributes) { _%>
+                    <%_ if (attr.datatype === 'RELATION' && attr.datatypeOptions.relationType === 'OWNS_MANY') continue _%>
+                    <%_ if (attr.help) { _%>
+                    <th>
+                      <%= attr.label %>
+                      <i className="fa fa-fw fa-question-circle-o"></i>
+                      { /* TODO tooltip on bottom attr.help */ }
+                    </th>
+                    <%_ } else { _%>
+                    <th><%= attr.label %></th>
+                    <%_ } _%>
+                    <%_ } _%>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    (this.state.collection.length === 0) ? (
+                      <tr className="tr-warning">
+                        <%_ for (index in schema.attributes) { _%>
+                        <%_ if (index === '0') { _%>
+                        <td>Empty</td>
+                        <%_ } else { _%>
+                        <td></td>
+                        <%_ } _%>
+                        <%_ } _%>
+                        <td></td>
+                      </tr>
+                    ) : (
+                      this.state.collection.map((model) => {
+                        return <<%- schema.class_name %>Preview model={model} onDeleteItem={this.deleteItem} key={model._id} />
+                      })
+                    )
+                  }
+                </tbody>
+              </table>
+            </Loader>
           </div>
         </div>
       </div>
@@ -65,4 +105,3 @@ class <%- schema.class_name %>List extends Component {
 }
 
 export default <%- schema.class_name %>List;
-
